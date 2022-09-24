8b8a781da8c1e709ebaf246f6f829984da4fce00
Deepa Bhandari <me.dpabhandari@gmail.com>
2022-09-24 15:41:43 +0545

    installation of tailwind css

---
+++ starter-code/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-3xl text-blue-300 font-bold underline">
+      Hello world!
+    </h1>
+    
+    </div>
+  );
+}
+
+export default App;
